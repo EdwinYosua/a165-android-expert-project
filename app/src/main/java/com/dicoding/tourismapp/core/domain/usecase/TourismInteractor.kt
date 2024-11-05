cf06f6e846468f70257f91fb29feba2d42c43ed8
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-11-05 18:44:10 +0800

    add usecase and interactor

---
+++ app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismInteractor.kt
@@ -0,0 +1,15 @@
+package com.dicoding.tourismapp.core.domain.usecase
+
+import androidx.lifecycle.LiveData
+import com.dicoding.tourismapp.core.data.Resource
+import com.dicoding.tourismapp.core.domain.model.Tourism
+import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
+
+class TourismInteractor(private val repository: ITourismRepository) : TourismUseCase {
+    override fun getAllTourism(): LiveData<Resource<List<Tourism>>> = repository.getAllTourism()
+
+    override fun getFavoriteTourism(): LiveData<List<Tourism>> = repository.getFavoriteTourism()
+
+    override fun setFavoriteTourism(tourism: Tourism, state: Boolean) =
+        repository.setFavoriteTourism(tourism, state)
+}
