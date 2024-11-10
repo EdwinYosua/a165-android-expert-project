5cace0ea22d1bca9e72a0f9634bd19ddecb1f68d
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-11-10 12:41:05 +0800

    Update HomeViewModel.kt

--- app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
@@ -1,11 +1,12 @@
 package com.dicoding.tourismapp.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 
 class HomeViewModel(tourismRepository: TourismUseCase) : ViewModel() {
 
-    val tourism = tourismRepository.getAllTourism()
+    val tourism = tourismRepository.getAllTourism().asLiveData()
 
 }
 
