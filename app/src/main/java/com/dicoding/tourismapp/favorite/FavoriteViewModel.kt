0d9cdd5595b0b01e17bdf3599061f45db0ab7ad6
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-11-10 12:41:10 +0800

    Update FavoriteViewModel.kt

--- app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
@@ -1,11 +1,12 @@
 package com.dicoding.tourismapp.favorite
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 
 class FavoriteViewModel(tourismRepository: TourismUseCase) : ViewModel() {
 
-    val favoriteTourism = tourismRepository.getFavoriteTourism()
+    val favoriteTourism = tourismRepository.getFavoriteTourism().asLiveData()
 
 }
 
